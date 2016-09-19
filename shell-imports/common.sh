b8c13d5890d66a3fdbda07eb356745dd8953daf9
Tim Lawson <timlawso@amazon.com>
2016-09-19 09:07:49 -0700

    Initial commit for common dotfiles

---
+++ shell-imports/common.sh
@@ -0,0 +1,10 @@
+bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+export KEYTIMEOUT=1
+
