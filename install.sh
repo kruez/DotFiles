3475475a3bc326d2b1d627dc90d4eb7637dc12a6
Tim Lawson <timlawso@amazon.com>
2016-09-20 08:02:41 -0700

    Automatically install Homebrew if not present

--- install.sh
+++ install.sh
@@ -16,6 +16,13 @@ if [ "$(uname)" == "Darwin" ]; then
     printf "source \"%s\"\n" "$DOT_ROOT/shell-imports/mac.sh" >> $HOME/.zshrc
   fi
 
+  # Install Homebrew if not present
+  which -s brew
+  if [[ $? != 0 ]]; then
+    # Install brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
   # Install oh-my-zsh if not present already
   if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing oh-my-zsh..."
