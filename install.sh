27da7b1f174dedb4a9beb5bc0cadfab88c5a80df
Tim Lawson <timlawso@amazon.com>
2016-09-19 20:12:58 -0700

    Add bootstrapping of mac imports script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Starting personal DotFile configuration"
+
 DOT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Append import of common shell config
@@ -7,3 +9,12 @@ if [ -z "$TJL_COMMON_IMPORTED" ]; then
   echo "Adding common shell-imports..." 
   printf "source \"%s\"\n" "$DOT_ROOT/shell-imports/common.sh" >> $HOME/.zshrc
 fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [ -z "$TJL_MAC_IMPORTED" ]; then
+    echo "Adding mac shell-imports..."
+    printf "source \"%s\"\n" "$DOT_ROOT/shell-imports/mac.sh" >> $HOME/.zshrc
+  fi
+fi
+
+echo "Personal DotFile configuration complete"
