e3ce0784861b1bedd8e9ab1d39d7c3c107be5838
Tim Lawson <timlawso@amazon.com>
2016-11-28 10:00:05 -0800

    Symlink in git config to home

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@ echo "Starting personal DotFile configuration"
 
 DOT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+HOME_ROOT=$HOME
+
+echo "Using following as home root: $HOME_ROOT"
+
 # Symlink in certain config files
-ln -sfv "$DOT_ROOT/editors/.vimrc" ~/.vimrc
-ln -sfv "$DOT_ROOT/git/.gitignore_global" ~
+ln -sfv "$DOT_ROOT/editors/.vimrc" $HOME_ROOT/.vimrc
+ln -sfv "$DOT_ROOT/git/.gitignore_global" $HOME_ROOT
+ln -sfv "$DOT_ROOT/git/.gitconfig" $HOME_ROOT
 
 if [ "$(uname)" == "Darwin" ]; then
   
@@ -18,7 +23,7 @@ if [ "$(uname)" == "Darwin" ]; then
   fi
 
   # Install oh-my-zsh if not present already
-  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  if [ ! -d "$HOME_ROOT/.oh-my-zsh" ]; then
     #TODO Perform this install on both Mac and Cloud
     echo "Installing oh-my-zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -28,7 +33,7 @@ if [ "$(uname)" == "Darwin" ]; then
   if [ "$1" != "" ]; then
     #TODO Make command line argument recognition smarter
     echo "Symlinking mac.sh to home dir as .zshrc"
-    ln -sfv "$DOT_ROOT/shell-imports/mac.sh" $HOME/.zshrc
+    ln -sfv "$DOT_ROOT/shell-imports/mac.sh" $HOME_ROOT/.zshrc
   fi
 fi
 
