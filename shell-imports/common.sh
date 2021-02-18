3be3a41020ef6e9265075106348e16613eed64bd
🦄  Tim Lawson <timlawso@amazon.com>
2021-02-17 22:10:20 -0800

    Move oh-my-zsh plugin definition to common source

--- shell-imports/common.sh
+++ shell-imports/common.sh
@@ -48,6 +48,13 @@ export ZSH=$HOME/.oh-my-zsh
 # Oh-my-zsh Theme Settings
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+# REMEMBER: zsh-syntax-highlighting MUST be last
+plugins=(git zsh-syntax-highlighting)
+
 source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh
