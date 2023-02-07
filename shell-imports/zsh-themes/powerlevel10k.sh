050e2a5ebdc8bad97f5775b6701e7b3ecced221c
🦄  Tim Lawson <timlawso@amazon.com>
2023-02-07 12:24:44 -0800

    Further customize terminal colors

--- shell-imports/zsh-themes/powerlevel10k.sh
+++ shell-imports/zsh-themes/powerlevel10k.sh
@@ -1,2 +1,22 @@
 ZSH_THEME="powerlevel10k/powerlevel10k"
 source $CUSTOM_ZSH_THEMES/powerlevel10k-config.sh
+
+# These customizations must be set after sourcing the above config file!
+
+typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=38
+typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
+typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=31
+
+typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_COLOR=172
+
+# Green prompt symbol if the last command succeeded.
+typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=148
+
+# Red prompt symbol if the last command failed.
+typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=203
+
+typeset -g POWERLEVEL9K_STATUS_ERROR=true
+
+# NOTE: Some Git color customizations are occurring within the my_git_formatter function
+# in the config file.
+# clean => 112
